b22927826184817028b4b87c84f8f0f2596c92d7
alonso <alons182@hotmail.com>
2013-11-21 13:30:13 -0600

    init green joomla

---
+++ templates/greenlife/index.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  Templates.3monkiescr
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+
+
+$app = JFactory::getApplication();
+$doc = JFactory::getDocument();
+$this->language = $doc->language;
+
+
+$itemid   = $app->input->getCmd('Itemid', '');
+
+// Add JavaScript Frameworks
+//JHtml::_('bootstrap.framework');
+
+// Add Stylesheets
+$doc->addStyleSheet('templates/'.$this->template.'/css/normalize.min.css');
+$doc->addStyleSheet('templates/'.$this->template.'/css/main.css');
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="<?php echo $this->language; ?>" >
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<jdoc:include type="head" />
+     <!--<link rel="icon" type="image/png" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/favicon_32x32.ico">-->
+	<script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/vendor/modernizr-2.6.2.min.js"></script>
+     
+     
+</head>
+
+<body class="<?php echo ($itemid ? ' bgid-' . $itemid : '')?>">
+	
+	   <header id="main-header">
+            <div class="inner">
+                <div id="logo">
+                    <a href="<?php echo $this->baseurl ?>"><img src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/img/logo.png" alt="Green Life Tour" /></a>
+                </div>
+                <div class="right-info">
+                    <div id="redes">
+                        <a href="#" class="icon-facebook"></a>
+                        <a href="#" class="icon-twitter"></a>
+                        
+                    </div>
+                    <jdoc:include type="modules" name="btn-reserve" style="none" />
+                    <a href="#" id="btn-reserve">Reserve Now</a>
+                    <div id="btn_nav"><span class="icon-menu"></span>Menu</div>
+                    <nav id="menu">
+                        <jdoc:include type="modules" name="menu" style="none" />
+                    </nav>
+                </div>
+                
+            </div>
+        </header>
+        <div id="main">
+                     
+            <jdoc:include type="component" />
+            
+
+        </div>
+        <footer id="main-footer">
+            <div id="copyright">
+                <a href="http://www.avotz.com" target="_blank">Copyright &copy; 2013 | Avotz</a >
+            </div>
+        </footer>
+
+       
+        <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/utils.js"></script>
+        <script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/main.js"></script>
+    
+        <script>
+           /* var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src='//www.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));*/
+        </script>
+        
+
+	<jdoc:include type="modules" name="debug" style="none" />
+</body>
+</html>
---
+++ templates/greenlife/js/main.js
@@ -0,0 +1,90 @@
+jQuery(function($) {
+
+
+    resizes();                     // resize pagina
+    $(window).scroll(resizes);     // calcula el desplazamiento de la pagina
+    $(window).resize(resizes);     // calcula el cambio de tamaño de la pagina
+  
+     // NAV MOBILE
+    $('#btn_nav').click(function(){
+        $('#main-header nav').toggle();
+    });
+
+    $(".tour").hover(function(event){
+
+      console.log($(this).attr("id"));
+      var myitem = $(this);
+      var por_left = myitem[0].style.left;
+      var pix_left = myitem[0].style.left;
+     
+      switch($(this).attr("id")) {
+          case 't1': 
+                $('#t2').animate({"left":"70%"});
+                $('#t3').animate({"left":"80%"});
+                $('#t4').animate({"left":"90%"});
+                
+            break;
+          case 't2': 
+                if ( por_left == '10%' || por_left=='' ) {
+                    $('#t3').animate({"left":"80%"});
+                    $('#t4').animate({"left":"90%"});
+                    $('#t2').animate({"left":"10%"});
+                    
+                }
+                else {
+                    $('#t2').animate({"left":"10%"});
+                   
+                  }
+                  
+            break;
+          case 't3': 
+             if ( por_left == '20%' || por_left=='' ) {
+                    $('#t4').animate({"left":"90%"});
+                    $('#t3').animate({"left":"20%"});
+                    $('#t2').animate({"left":"10%"});
+                   
+                }
+                else {
+                    $('#t3').animate({"left":"20%"});
+                    $('#t2').animate({"left":"10%"});
+                   
+                  }
+                 
+            break;
+           case 't4': 
+                if ( por_left == '90%' || por_left=='' ) {
+                    $('#t4').animate({"left":"30%"});
+                    $('#t3').animate({"left":"20%"});
+                    $('#t2').animate({"left":"10%"});
+                 }
+                 
+            break;
+        } 
+    },function(){
+      
+    });
+
+   
+
+    function resizes(){
+            height_dispo = getWindowHeight() - ($('#main-header').height()) - ($('.item-page').height()) - ($('#main-footer').height());
+            width_dispo = getWindowWidth() - getScrollerWidth();            
+           
+
+            if(getWindowWidth() > 768){
+                
+                 $('#main').height(height_dispo).width(width_dispo);
+                 $('.item-page_tours_home li').height(height_dispo);//.width(width_dispo);
+               
+            }else
+              {  
+               $('#main').height('auto').width('auto');
+               $('.item-page_tours_home li').height('500px');
+            }
+           
+
+
+    };
+
+});
+
