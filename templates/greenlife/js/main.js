7c0ff73328218b6f0fe2af4b3e5caa4bbe7f01b7
alonso <alons182@hotmail.com>
2014-01-20 11:35:33 -0600

    boton trans

--- templates/greenlife/js/main.js
+++ templates/greenlife/js/main.js
@@ -239,7 +239,7 @@ jQuery(function($) {
 
             if(getWindowWidth() > 768){
                 
-                 if(getWindowHeight() >= 679 && getWindowHeight() <= 768){
+                 if(getWindowHeight() >= 600 && getWindowHeight() <= 768){
                   $('#main').height(height_dispo + 90).width(width_dispo);
                  }
                  else
