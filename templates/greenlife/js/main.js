8ff59e78110fdd6eb59563191d0d55b55754f9f9
alonso <alons182@hotmail.com>
2014-04-22 16:57:10 -0600

    submenu hover

--- templates/greenlife/js/main.js
+++ templates/greenlife/js/main.js
@@ -119,7 +119,11 @@ jQuery(function($) {
 
         $('#dialog').fadeIn(200);
 
-    }else{
+    }else if($(this).attr('data-info')=="rates")
+    {
+      console.log('desabilitado');
+    }
+    else{
        $(".info").find('article').hide();
        $(".info").find('[data-id="'+ $(this).attr('data-info') +'"]').fadeIn(500);
     }
